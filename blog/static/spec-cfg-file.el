affb126a7131a8282f7110627e60aeea8de36229
Katherine Cox-Buday <cox.katherine.e@gmail.com>
2015-03-02 10:18:17 -0600

    Update published html files, committed by org-page.

---
+++ blog/static/spec-cfg-file.el
@@ -0,0 +1,27 @@
+my-style	(ref:stylesheet-name)	\documentclass{./my-style} (ref:stylesheet-loc)
+                 [DEFAULT-PACKAGES]
+                 [PACKAGES]
+                 [EXTRA]	("\\section{%s}" . "\\section{%s}")	("\\subsection{%s}" . "\\subsection{%s}")	("\\subsubsection{%s}" . "\\subsubsection{%s}")	("\\paragraph{%s}" . "\\paragraph{%s}")	("\\subparagraph{%s}" . "\\subparagraph{%s}")
+my-style	(ref:stylesheet-name)	\documentclass{~/Dropbox/canonical/style/rga}
+                 [DEFAULT-PACKAGES]
+                 [PACKAGES]
+                 [EXTRA]	("\\section{%s}" . "\\section{%s}")	("\\subsection{%s}" . "\\subsection{%s}")	("\\subsubsection{%s}" . "\\subsubsection{%s}")	("\\paragraph{%s}" . "\\paragraph{%s}")	("\\subparagraph{%s}" . "\\subparagraph{%s}")
+rga	\documentclass{~/Dropbox/canonical/style/rga}
+                 [DEFAULT-PACKAGES]
+                 [PACKAGES]
+                 [EXTRA]	("\\section{%s}" . "\\section{%s}")	("\\subsection{%s}" . "\\subsection{%s}")	("\\subsubsection{%s}" . "\\subsubsection{%s}")	("\\paragraph{%s}" . "\\paragraph{%s}")	("\\subparagraph{%s}" . "\\subparagraph{%s}")
+resume	\documentclass{resume}
+               [NO-DEFAULT-PACKAGES]
+               [PACKAGES]
+               [EXTRA]	("\\section{%s}" . "\\section{%s}")	("\\subsection{%s}" . "\\subsection{%s}")	("\\subsubsection{%s}" . "\\subsubsection{%s}")	("\\paragraph{%s}" . "\\paragraph{%s}")	("\\subparagraph{%s}" . "\\subparagraph{%s}")
+canonical	\documentclass{canonical}
+               [DEFAULT-PACKAGES]
+               [PACKAGES]
+               [EXTRA]	("\\section{%s}" . "\\section{%s}")	("\\subsection{%s}" . "\\subsection{%s}")	("\\subsubsection{%s}" . "\\subsubsection{%s}")	("\\paragraph{%s}" . "\\paragraph{%s}")	("\\subparagraph{%s}" . "\\subparagraph{%s}")
+rga	\documentclass{~/Dropbox/canonical/style/rga}
+               [DEFAULT-PACKAGES]
+               [PACKAGES]
+               [EXTRA]	("\\section{%s}" . "\\section{%s}")	("\\subsection{%s}" . "\\subsection{%s}")	("\\subsubsection{%s}" . "\\subsubsection{%s}")	("\\paragraph{%s}" . "\\paragraph{%s}")	("\\subparagraph{%s}" . "\\subparagraph{%s}")
+article	\documentclass[11pt]{article}	("\\section{%s}" . "\\section*{%s}")	("\\subsection{%s}" . "\\subsection*{%s}")	("\\subsubsection{%s}" . "\\subsubsection*{%s}")	("\\paragraph{%s}" . "\\paragraph*{%s}")	("\\subparagraph{%s}" . "\\subparagraph*{%s}")
+report	\documentclass[11pt]{report}	("\\part{%s}" . "\\part*{%s}")	("\\chapter{%s}" . "\\chapter*{%s}")	("\\section{%s}" . "\\section*{%s}")	("\\subsection{%s}" . "\\subsection*{%s}")	("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+book	\documentclass[11pt]{book}	("\\part{%s}" . "\\part*{%s}")	("\\chapter{%s}" . "\\chapter*{%s}")	("\\section{%s}" . "\\section*{%s}")	("\\subsection{%s}" . "\\subsection*{%s}")	("\\subsubsection{%s}" . "\\subsubsection*{%s}")
